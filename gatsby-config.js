20164d4eb3ab05541e3df54bb7e567f7c943ce0b
Victor <giroud.victor@gmail.com>
2021-05-11 13:16:02 +0200

    Fix url: .com not .app

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
   siteMetadata: {
     title: "Amélie La Vie",
     author: "Victor Giroud",
-    siteURL: "https://amelielavie.app",
+    siteURL: "https://amelielavie.com",
     description:
       'L\'association "Amélie la vie" a pour but de venir en aide à des personnes atteintes de maladies engageant le pronostic vital ou fortement handicapantes.',
   },
